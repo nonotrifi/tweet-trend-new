f8b81f92551aef98b4cc7d02b8bce95169040e21
ntrifi <trifinourdine@outlook.fr>
2024-02-12 20:11:49 +0100

    added html page

--- src/main/java/com/valaxy/demo/DemoWorkshopApplication.java
+++ src/main/java/com/valaxy/demo/DemoWorkshopApplication.java
@@ -1,5 +1,4 @@
 package com.stalin.demo;
-package com.valaxy.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +11,3 @@ public class DemoWorkshopApplication{
 	}
 
 }
-
-
-
